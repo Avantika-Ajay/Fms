205bbbc196d6d5d08b7b34d1951a6b0cbedecfaa
Avantika-Ajay <122859842+Avantika-Ajay@users.noreply.github.com>
2024-03-09 23:06:38 +0530

    Login flow complete

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const dbService = require('./dbService.js');
+const {con} = require('./dbService.js');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.post('/login-api',(req,res)=>{
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // console.log(con);
+
+
+    con.query("SELECT * FROM users where username='"+username+"' and password='"+password+"'", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        if(result.length == 0){
+            res.status(400).send("Error, user doesnot exist");
+        } else {
+            res.status(200).render('index');
+        }
+      });
+})
+
 
 // Login route
 // app.get('/login/:username/:password', (req, res) => {
--- dbService.js
+++ dbService.js
@@ -3,10 +3,13 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: 'password'
+  password: 'password',
+  database:'fms'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+module.exports.con = con;
